972d28b14de1bc9ca8255b83cbe11281f3ff216b
Geiliane777 <geiliane.tomaz@odontogroup.com.br>
2026-09-04 14:00:11 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,9 +1,53 @@
-// =============================
+// ======================================
+// SUPABASE.JS
 // CONFIGURAÇÃO DO SUPABASE
-// =============================
-const SUPABASE_URL = "https://mmmiqqxienjeioxcnggu.supabase.co";
-const SUPABASE_KEY = "sb_publishable_ZHE8lc4B6IgyqDLe_LRK7w_edpWgmSz";
-const supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
+// ======================================
+
+console.log("supabase.js carregado");
+
+
+// ======================================
+// CONFIGURAÇÕES
+// ======================================
+
+const SUPABASE_URL =
+    "https://mmmiqqxienjeioxcnggu.supabase.co";
+
+const SUPABASE_KEY =
+    "sb_publishable_ZHE8lc4B6IgyqDLe_LRK7w_edpWgmSz";
+
+
+// ======================================
+// VERIFICAR BIBLIOTECA
+// ======================================
+
+if (!window.supabase) {
+
+    console.error(
+        "Biblioteca do Supabase não foi carregada!"
+    );
+
+} else {
+
+    console.log(
+        "Biblioteca Supabase carregada com sucesso."
+    );
+
+}
+
+
+// ======================================
+// CRIAR CLIENTE
+// ======================================
+
+const supabaseClient =
+    window.supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_KEY
+    );
+
+
+console.log(
+    "supabaseClient criado com sucesso:",
+    supabaseClient
 );
