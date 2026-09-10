102ae6b82ee6d780c8f3ebf12a02c8eb39b7001a
Geiliane777 <geiliane.tomaz@odontogroup.com.br>
2026-09-10 16:29:10 -0300

    Fix CSS reset syntax in especialistas.js

--- js/especialistas.js
+++ js/especialistas.js
@@ -2,7 +2,7 @@
    RESET + VARIÁVEIS
 ========================================================= */
 
-*{margin:0;padding:0;box-sizing:border-box}
+{margin:0;padding:0;box-sizing:border-box}
 html{scroll-behavior:smooth}
 
 :root{
